d4421421937135852e1e556a7e3702f7c81ce810
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-06-09 17:39:34 +0800

    completed MVP

--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,28 @@
 const db = require("../database/db-config");
 
-function addUser(user) {
-  return db("users").insert(user);
+function find() {
+  return db("users").select("id", "username");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function addUser(user) {
+  const [id] = await db("users").insert(user, "id");
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
 }
 
 module.exports = {
-  addUser
+  find,
+  findBy,
+  addUser,
+  findById
 };
--- users/users-router.js
+++ users/users-router.js
@@ -3,4 +3,15 @@ const Users = require("./users-model");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "Database error" });
+    });
+});
+
 module.exports = router;
